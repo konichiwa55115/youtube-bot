5d35c4fc18a47e02c5122ab576d4253d274fc621
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2022-12-29 18:24:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
-    DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    bot_token = ""
+    api_id = ""
+    api_hash = ""
+   
