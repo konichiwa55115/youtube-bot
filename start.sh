d3b016dbf64fd923ed4edcda749517a601a68eeb
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2022-12-29 20:03:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/youtube-bot /LazyDeveloper
 cd /LazyDeveloper
+mkdir playlists
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
