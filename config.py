ace67403c1a751d399f95948bf9c3e23abb694a9
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2022-12-29 18:25:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class config:
-    bot_token = ""
-    api_id = ""
-    api_hash = ""
+    bot_token = "5793890668:AAG0Tl-27fOWdy5cfCDd7Zfwo5qaOAM_n6o"
+    api_id = "17983098"
+    api_hash = "ee28199396e0925f1f44d945ac174f64"
    
